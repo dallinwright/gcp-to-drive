fb6350fbcc622b8162e9099a354a4d00f3740abc
Dallin Wright <dallinwright01@gmail.com>
2023-02-12 21:07:34 +0100

    working file download

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import os
-import traceback
-
-import google.auth
 from google.cloud import storage
-from googleapiclient import discovery
-from googleapiclient.http import MediaFileUpload
 
 # Run beforehand this command
 # gcloud storage ls -r gs://test-bucket-99099099/ | grep -i "mp3" > files.txt
@@ -13,19 +7,12 @@ BUCKET_FOLDER = "/"
 EXTENSION = ".mp3"
 INPUT_FILE = "files.txt"
 
+import google.auth
 
-def upload_to_drive(local_path, remote_path):
-    GDRIVE_FOLDER_IDS = str(os.environ["GDRIVE_FOLDER_IDS"]).split(",")
-    credentials, _ = google.auth.default(scopes=["https://www.googleapis.com/auth/drive"])
-    service = discovery.build('drive', 'v3', credentials=credentials)
-    media = MediaFileUpload(local_path)
-    print("Starting to upload in the drive")
-    response = service.files().create(media_body=media,
-                                      body={"name": remote_path.split("/")[-1], "parents": GDRIVE_FOLDER_IDS}).execute()
-    print(response)
+credentials, project = google.auth.default()
 
 
-def download_blob(bucket_name, source_blob_name, destination_file_name):
+def download_blob(source_blob_name, destination_file_name):
     """Downloads a blob from the bucket."""
     # The ID of your GCS bucket
     # bucket_name = "your-bucket-name"
@@ -38,7 +25,7 @@ def download_blob(bucket_name, source_blob_name, destination_file_name):
 
     storage_client = storage.Client()
 
-    bucket = storage_client.bucket(bucket_name)
+    bucket = storage_client.bucket(BUCKET)
 
     # Construct a client side representation of a blob.
     # Note `Bucket.blob` differs from `Bucket.get_blob` as it doesn't retrieve
@@ -49,28 +36,28 @@ def download_blob(bucket_name, source_blob_name, destination_file_name):
 
     print(
         "Downloaded storage object {} from bucket {} to local file {}.".format(
-            source_blob_name, bucket_name, destination_file_name
+            source_blob_name, BUCKET, destination_file_name
         )
     )
 
 
-def drive_upload(event, context):
-    try:
-        if event["name"][-1] == "/":
-            print("Folder: {} is created in the bucket: {}".format(
-                event["name"],
-                event["bucket"]
-            ))
-            return
-
-        # Downloading file from GCS
-        local_path = download_file_from_gcs(event["bucket"], event["name"])
-
-        # Uploading the file to drive
-        upload_to_drive(local_path, event["name"])
-
-    except Exception as e:
-        print(traceback.format_exc())
+# def drive_upload(event, context):
+#     try:
+#         if event["name"][-1] == "/":
+#             print("Folder: {} is created in the bucket: {}".format(
+#                 event["name"],
+#                 event["bucket"]
+#             ))
+#             return
+#
+#         # Downloading file from GCS
+#         local_path = download_file_from_gcs(event["bucket"], event["name"])
+#
+#         # Uploading the file to drive
+#         upload_to_drive(local_path, event["name"])
+#
+#     except Exception as e:
+#         print(traceback.format_exc())
 
 
 def main():
@@ -89,14 +76,15 @@ def main():
     while len(lines) > 0:
         line = lines[0]
 
+        line = line.replace(f"gs://{BUCKET}/", "")
+
         # Helpful progress display
         print(f"Processing line: {index} of {initial_length} ({round((index / initial_length) * 100, 2)}%)")
 
         filename = line.split("/")[-1]
-        os.mkdir(f"./files")
         destination = f"./files/{filename}"
 
-        download_blob(BUCKET, line, destination)
+        download_blob(line, destination)
 
         # Keeps track and removes lines as they are processed. If the program dies, it can start from where it left off.
         lines.pop(0)
