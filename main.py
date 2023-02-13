8a79aae4003f16b253d7c5d3e94abc4f7e81a2f3
Dallin Wright <dallinwright01@gmail.com>
2023-02-13 11:00:41 +0100

    finalized logic

--- main.py
+++ main.py
@@ -51,46 +51,6 @@ def download_blob(bucket_name, source_blob_name, destination_file_name):
     )
 
 
-def upload_basic(filename: str):
-    """Insert new file.
-    Returns : Id's of the file uploaded
-
-    Load pre-authorized user credentials from the environment.
-    TODO(developer) - See https://developers.google.com/identity
-    for guides on implementing OAuth2 for the application.
-    """
-    mp4_mime_type = "video/mp4"
-    avi_mime_type = "video/x-msvideo"
-    webm_mime_type = "video/webm"
-
-    selected_mime_type = ""
-
-    if filename.endswith(".mp4"):
-        selected_mime_type = mp4_mime_type
-    elif filename.endswith(".avi"):
-        selected_mime_type = avi_mime_type
-    elif filename.endswith(".webm"):
-        selected_mime_type = webm_mime_type
-
-    try:
-        # create drive api client
-        service = build('drive', 'v3', credentials=credentials)
-
-        file_metadata = {'name': filename}
-        media = MediaFileUpload(filename,
-                                mimetype=selected_mime_type)
-        # pylint: disable=maybe-no-member
-        file = service.files().create(body=file_metadata, media_body=media,
-                                      fields='id').execute()
-        print(F'File ID: {file.get("id")}')
-
-    except HttpError as error:
-        print(F'An error occurred: {error}')
-        file = None
-
-    return file.get('id')
-
-
 def main():
     lines = []
 
